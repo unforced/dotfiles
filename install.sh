53f99221830d0d54761f7865ece6e70cd9bf0a03
Aaron Neyer <aaronneyer@gmail.com>
2013-01-29 14:07:21 -0500

    Some cleanup

--- install.sh
+++ install.sh
@@ -26,18 +26,13 @@ install_symlink() {
 
 ####
 
-for dotfile in `ls | grep -v install | grep -v custom_scripts | grep -v sshconfig`; do
+for dotfile in `ls | grep -vE "install|sshconfig|README"`; do
   install_symlink "$HOME/.${dotfile}" $DOTDIR/$dotfile
-done
-
-install_symlink "$HOME/.ssh/config" $DOTDIR/sshconfig
-
-for script in `ls custom_scripts`; do
-  sudo ln -s "$DOTDIR/custom_scripts/$script" /usr/local/bin/$script
 done
 
 if which git > /dev/null; then
   git submodule update --init
+  git submodule update #Was being buggy for some reason so added this
   git config --global core.excludesfile $HOME/.gitignore
 else
   echo "Please install git and run 'git submodule update --init'"
