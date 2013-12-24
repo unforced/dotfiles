5a5ba22bc04c059c53de4a3e8d02acc5548cb016
Aaron Neyer <aaronneyer@gmail.com>
2013-12-24 12:07:08 -0500

    add readline to installs, and show username in prompt

--- ubuntu-installs.sh
+++ ubuntu-installs.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get install -y ruby git tmux vim zsh
+sudo apt-get install -y ruby git tmux vim zsh libreadline-dev
 
 git clone git://github.com/sstephenson/rbenv ~/.rbenv
 git clone git://github.com/sstephenson/ruby-build ~/.rbenv/plugins/ruby-build
