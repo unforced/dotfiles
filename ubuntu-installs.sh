25c6aac51dfcb841b71d2fcf641d046c39130f87
Aaron Neyer <aaronneyer@gmail.com>
2014-04-15 19:24:26 -0400

    rbenv dependencies in ubuntu-installs and fix ssh config

--- ubuntu-installs.sh
+++ ubuntu-installs.sh
@@ -1,6 +1,9 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get install -y ruby git tmux vim zsh libreadline-dev
+sudo apt-get install -y ruby git tmux vim zsh curl
 
 git clone git://github.com/sstephenson/rbenv ~/.rbenv
 git clone git://github.com/sstephenson/ruby-build ~/.rbenv/plugins/ruby-build
+
+# rbenv dependencies
+sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev
